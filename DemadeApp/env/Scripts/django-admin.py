11fb3792040e521383eeb3c399172ee506567988
NUHU <souleymaneeladjnuhu@gmail.com>
2021-04-05 19:04:35 +0000

    Ajoutez des fichiers projet.

---
+++ DemadeApp/env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\soule\source\repos\DemadeApp\DemadeApp\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
